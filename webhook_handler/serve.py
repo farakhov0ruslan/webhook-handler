3b08d04c4882e52dfc6fe8bc30a330dd8ea88052
ruslanfarahov <farakhov0ruslan@gmail.com>
2026-05-05 14:29:17 +0300

    chore:

--- webhook_handler/serve.py
+++ webhook_handler/serve.py
@@ -43,6 +43,7 @@ def serve(env: str = "local") -> None:
 
 
 if __name__ == "__main__":  # pragma: no cover
-    configure_logger("webhook_handler", "INFO", json_logger=True)
+    configure_logger("webhook_handler", "debug", json_logger=True)
+    configure_logger("utils_library", "INFO", json_logger=True)
     configure_logger(__name__, "INFO", json_logger=True)
     fire.Fire(serve)
